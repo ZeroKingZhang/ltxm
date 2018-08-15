3bd8187150698dd9b3007597d54c5365681d9c00
ZeroKingZhang <260038686@qq.com>
2018-08-15 11:03:01 +0800

    修改冲突

--- resources/views/admin/layout/index.blade.php
+++ resources/views/admin/layout/index.blade.php
@@ -114,7 +114,6 @@
 							<a href="#subPages2" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>版块管理</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
 							<div id="subPages2" class="collapse ">
 								<ul class="nav">
-<<<<<<< HEAD
 									<li><a href="/admin/forum/create" class="">版块添加</a></li>
 									<li><a href="/admin/forum" class="">版块显示</a></li>
 									<li><a href="/admin/forum" class="">回收站</a></li>
@@ -128,16 +127,13 @@
 									
 									<li><a href="/admin/carousel" class="">轮播图显示</a></li>
 									<li><a href="/admin/carousel/create" class="">轮播图上传</a></li>
-									<li><a href="/forum/create" class="">版块添加</a></li>
-									<li><a href="/forum" class="">版块显示</a></li>
-									
 								</ul>
 							</div>
 
 						</li>
 						<li>
-							<a href="#subPages3" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>敏感词管理</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
-							<div id="subPages3" class="collapse ">
+							<a href="#subPages4" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>敏感词管理</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
+							<div id="subPages4" class="collapse ">
 								<ul class="nav">
 									<li><a href="/sensitivity/create" class="">敏感词添加</a></li>
 									<li><a href="/sensitivity" class="">敏感词浏览</a></li>
@@ -147,8 +143,8 @@
 
 						</li>
 						<li>
-							<a href="#subPages4" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>友情链接管理</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
-							<div id="subPages4" class="collapse ">
+							<a href="#subPages5" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>友情链接管理</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
+							<div id="subPages5" class="collapse ">
 								<ul class="nav">
 									<li><a href="/blogroll/create" class="">友情链接添加</a></li>
 									<li><a href="/blogroll" class="">友情链接浏览</a></li>	
